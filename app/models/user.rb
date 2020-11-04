a02bd3120947f3b1d8b8e34fe08b62ef6e30c463
ayadenchu <saion.denchu316@gmail.com>
2020-11-04 15:26:43 +0900

    新規登録バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i }
   validates :nickname, presence: true
   validates :family_name,presence: true
   validates :first_name,presence: true
-  validates :family_name_kana,presence:true
-  validates :first_name_kana,presence:true
+  validates :family_name_kana,presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
+  validates :first_name_kana,presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/ }
   validates :birth_day,presence:true
 
   has_many :items
